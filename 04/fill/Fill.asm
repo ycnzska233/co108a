5abba08dffea52406bfe4c1cb746dbeb537ecb80
侯奕晨 <houyichen@houyichende-MacBook-Pro.local>
2020-01-09 02:15:00 +0800

    Ch4 fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,84 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(FOREVER) 
+
+//n = 8192
+    @8192
+    D=A //將位址8192存在D
+    @n
+    M=D //將8192存在n
+
+//arr = screen
+    @SCREEN
+    D=A
+    @arr
+    M=D
+//i = 0
+    @i
+    M=0 // 將i設為0
+
+//j = 0
+    @j
+    M=0 // 將j設為0
+
+            //while(i<n)
+            //=>if(i==n) goto ELSE
+//PrintLoop
+                (PrintLoop)
+                @i
+                D=M 
+                @n
+                D=D-M 
+                @ELSE
+                D;JEQ 
+
+            //if(*KBD != 0)
+                @KBD
+                D=M
+                @ELSE
+                D;JEQ 
+
+            //arr[i] = -1//因為-1在二進位中為1111111111111111,才會顯示全黑
+                @arr
+                D=M
+                @i
+                A=D+M
+                //@SCREEN
+                M=-1 
+
+            //i = i + 1
+                @i
+                M=M+1
+                @IFEND 
+                0;JMP
+
+//ELSE
+                (ELSE)
+                @j
+                D=M 
+                @n
+                D=D-M 
+                @END
+                D;JEQ 
+
+                @arr
+                D=M
+                @j
+                A=D+M
+                M=0
+                //j = j + 1
+                @j
+                M=M+1
+                @ELSE
+                0;JMP
+
+//IFEND
+                (IFEND)
+                @PrintLoop
+                0;JMP
+
+//END
+                (END)
+                @FOREVER
+                0;JMP
