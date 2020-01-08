c7ac2baf353961f116188aa2f0d1d264a361df3d
侯奕晨 <houyichen@houyichende-MacBook-Pro.local>
2020-01-09 02:20:43 +0800

    Ch4 mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@0
+D=A
+@2
+M=D
+(LOOP)
+@0
+D=M
+@END
+D;JLE
+@1
+D=M
+@2
+M=M+D
+@0
+M=M-1
+@LOOP
+0;JMP
+(END)
